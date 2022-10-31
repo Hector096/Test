f2289693b92245382f27473867645e498069508a
Hector <vv96rca@gmail.com>
2022-10-31 14:11:31 +0530

    aflow

--- src/attack.test.js
+++ src/attack.test.js
@@ -1,7 +1,8 @@
-import attack from './attack';
+import attack from "./attack";
 
-test('should return damage sentence when called correctly', () => {
-  const result = attack('Smelly Ogre', 23);
+test("should return damage sentence when called correctly", () => {
+  const result = attack("Smelly Ogre", 23);
   expect(result).toMatch(/^Smelly Ogre(.*)23/);
+  expect(result).not.toBe("Hello");
   expect(result).toMatchInlineSnapshot('"Smelly Ogre dealt 23 damage!"');
 });
